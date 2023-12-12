cd22a4f9d1bdff5217e0fb1f3525fca0a872792a
AnyaILi <135563965+AnyaILi@users.noreply.github.com>
2023-12-12 14:17:24 -0800

    Update index.py
    1st CNN code

--- index.py
+++ index.py
@@ -1,8 +1,80 @@
-import pandas as pd
-df = pd.DataFrame({'Number': {0: 1, 1: 2, 2: 3, 3: 4, 4: 5}, 
-                   'Power 2': {0: 1, 1: 4, 2: 9, 3: 16, 4: 25}, 
-                   'Power 3': {0: 1, 1: 8, 2: 27, 3: 64, 4: 125}}) 
-  
-# view dataframe 
-print("Initial dataframe")
-print(df)
+!pip install opencv-python
+!pip install tensorflow
+!pip install scikit-learn
+import os
+import cv2
+import numpy as np
+from time import time
+from tensorflow.keras import utils
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+%matplotlib inline
+crop = ['Cashew', 'Cassava', 'Maize', 'Tomato']
+train_dir = 'ASL-Data'
+test_dir = 'ASL-Data'
+classes = ['anthracnose', 'gumosis', 'healthy', 'leaf miner', 'red rust']
+plt.figure(figsize=(11, 11))
+for i in range (0,5):
+    plt.subplot(1,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    path = "Cashew" + train_dir + "0cashew_valid_anthracnose.JPG".format(classes[i])
+    img = plt.imread(path)
+    plt.imshow(img)
+    plt.xlabel(classes[i])
+
+def load_data(train_dir):
+    images = []
+    labels = []
+    size = 32,32
+    index = -1
+    for folder in os.listdir(train_dir):
+        index +=1
+        for image in os.listdir(train_dir + "/" + folder):
+            temp_img = cv2.imread(train_dir + '/' + folder + '/' + image)
+            temp_img = cv2.resize(temp_img, size)
+            images.append(temp_img)
+            labels.append(index)
+    
+    images = np.array(images)
+    images = images.astype('float32')/255.0
+    labels = utils.to_categorical(labels)
+    x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size = 15)
+    
+    print('Loaded', len(x_train),'images for training,','Train data shape =', x_train.shape)
+    print('Loaded', len(x_test),'images for testing','Test data shape =', x_test.shape)
+    
+    return x_train, x_test, y_train, y_test
+
+start = time()
+x_train, x_test, y_train, y_test = load_data(train_dir)
+print('Loading:', time() - start)
+
+# Creating model
+
+from keras import Sequbbbential
+from keras import layers
+
+model = Sequential([
+    layers.Rescaling(1./255),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.25),
+
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.25),
+    
+    layers.Conv2D(128, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.25),
+
+    layers.Flatten(),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(len(class_names), activation='softm'
